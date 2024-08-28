4fe8564f815f0f6b2a289d5721c59b3bf9172704
Hasan TEKGÜL <hasan.tekgul@tuik.gov.tr>
2024-08-28 16:35:18 +0300

    status rengi

--- src/commands/jobs/index.js
+++ src/commands/jobs/index.js
@@ -1,3 +1,4 @@
+import chalk from 'chalk'
 import { COLORS, createClient } from '../../lib/jenkins/config'
 import { createStandardTable } from '../../lib/table/standard-table'
 
@@ -8,13 +9,20 @@ function mapJob (job) {
   }
 }
 
+function prettyStatus (status) {
+  if (!status) return '-'
+  if (status === 'success') return chalk.green(status)
+  if (status === 'error') return chalk.red(status)
+  return chalk.gray(status)
+}
+
 export default async function jobs () {
   const jenkins = createClient()
   const res = await jenkins.get('/api/json')
   const jobs = res.data?.jobs?.map(mapJob)
   const table = createStandardTable({
     head: ['Name', 'Status'],
-    items: jobs.map(i => [i.name, i.status])
+    items: jobs.map(i => [i.name, prettyStatus(i.status)])
   })
   console.log(table.toString())
 }
